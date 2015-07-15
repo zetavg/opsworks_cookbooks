c7f6741e7c625d426ee216d4d4e013ea9d1c1ef4
Neson <neson@dex.tw>
2015-07-15 20:50:14 +0800

    update procfile worker recipe

--- procfile/recipes/worker.rb
+++ procfile/recipes/worker.rb
@@ -1,63 +1,60 @@
 node[:deploy].each do |application, _deploy|
-  ruby_block "Read Procfile and set the worker" do
-    block do
-      procfile_path = "#{node[:deploy][application][:deploy_to]}/current/Procfile"
 
-      worker_command = nil
+  procfile_path = "#{node[:deploy][application][:deploy_to]}/current/Procfile"
 
-      if File.exist?(procfile_path)
-        # Read the CWM version from file.
-        f = File.open(procfile_path)
+  worker_command = nil
 
-        pattern = /^worker: (?<command>.+)$/
+  if File.exist?(procfile_path)
 
-        f.each do |line|
-          if match = line.match(pattern)
-            worker_command = match['command']
-            break
-          end
-        end
+    f = File.open(procfile_path)
 
-        f.close
+    pattern = /^worker: (?<command>.+)$/
+
+    f.each do |line|
+      if match = line.match(pattern)
+        worker_command = match['command']
+        break
       end
+    end
 
-      if worker_command
-        template "#{node[:monit][:conf_dir]}/worker_#{application}.monitrc" do
-          owner 'root'
-          group 'root'
-          mode 0644
-          source "monitrc.conf.erb"
-          variables({
-            command: worker_command,
-            app_name: application,
-            process_type: 'worker',
-            process_count: 1
-          })
-        end
+    f.close
+  end
 
-        execute "Reload monit" do
-          command %Q{
-            monit reload
-          }
-        end
+  if worker_command
+    template "#{node[:monit][:conf_dir]}/worker_#{application}.monitrc" do
+      owner 'root'
+      group 'root'
+      mode 0644
+      source "monitrc.conf.erb"
+      variables({
+        command: worker_command,
+        app_name: application,
+        process_type: 'worker',
+        process_count: 1
+      })
+    end
 
-        execute "Restart worker" do
-          command %Q{
-            echo "sleep 20 && monit -g worker_#{application} restart all" | at now
-          }
-        end
+    execute "Reload monit" do
+      command %Q{
+        monit reload
+      }
+    end
 
-      else
-        execute "Stop worker" do
-          command %Q{
-            monit -g worker_#{application} stop all
-          }
-        end
+    execute "Restart worker" do
+      command %Q{
+        echo "sleep 20 && monit -g worker_#{application} restart all" | at now
+      }
+    end
 
-        file "#{node[:monit][:conf_dir]}/worker_#{application}.monitrc" do
-          action :delete
-        end
-      end
+  else
+    execute "Stop worker" do
+      command %Q{
+        monit -g worker_#{application} stop all
+      }
+    end
+
+    file "#{node[:monit][:conf_dir]}/worker_#{application}.monitrc" do
+      action :delete
     end
   end
 end
